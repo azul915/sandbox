e4d62836b9cb1f1c3804b3fb70ada28d16b45deb
azul915 <k.aoyagi940516@gmail.com>
2019-09-02 23:09:19 +0900

    LinkedList修正

--- src/com/aoyagi/kt/LinkedList.kt
+++ src/com/aoyagi/kt/LinkedList.kt
@@ -1,37 +1,37 @@
 package com.aoyagi.kt
 
+data class StationList(var name: String, var next: Int)
+
+var list = arrayOfNulls<StationList>(10)
+
+const val start = 2
+
 fun main(args: Array<String>) {
+    initStationList()
+    printStationList()
 
-    var list = arrayOfNulls<StationList>(5)
+    insertStationList(5, "品川", 2)
+    printStationList()
 
+    deleteStationList(5, 2)
+    printStationList()
+}
+
+fun initStationList() {
     list[0] = StationList("新大阪", -1)
     list[1] = StationList("名古屋", 3)
     list[2] = StationList("東京", 4)
     list[3] = StationList("京都", 0)
     list[4] = StationList("新横浜", 1)
-
-    val top = 2
-
-    var idx = top
-    while (idx != -1) {
-        print("[${list[idx]?.name}]->")
-        idx = list[idx]?.next!!
-    }
-    println("")
-
-
-}
-
-data class StationList(var name: String, var next: Int)
-
-
-fun initStationList() {
-
 }
 
 fun printStationList() {
-
-
+    var idx = start
+    while(idx != -1) {
+        print("[${list[idx]?.name!!}]->")
+        idx = list[idx]?.next!!
+    }
+    println("")
 }
 
 /*
@@ -40,11 +40,14 @@ fun printStationList() {
 @args preIdx 前の添字
  */
 fun insertStationList(insIdx: Int, insName: String, prevIdx: Int) {
-    list[insIdx]?.name = insName
-    list[insIdx]?.next = list[prevIdx]?.next
-    list[prevIdx]?.next = insIdx
+    list[insIdx] = StationList(insName, list[prevIdx]?.next!!)
+    list[prevIdx] = StationList(list[prevIdx]?.name!!, insIdx)
 }
 
+/*
+@args delIdx 削除する添字
+@args preIdx 前の添字
+ */
 fun deleteStationList(delIdx: Int, prevIdx: Int) {
-    list[delIdx]?.next = list[prevIdx]?.next
+    list[prevIdx] = StationList(list[prevIdx]?.name!!, list[delIdx]?.next!!)
 }
