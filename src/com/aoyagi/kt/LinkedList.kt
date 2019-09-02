be4e77f2538cb769773c1c44fa66092489e063a9
kosei.aoyagi <k.aoyagi@tcmobile.jp>
2019-09-02 14:06:17 +0900

    修正

--- src/com/aoyagi/kt/LinkedList.kt
+++ src/com/aoyagi/kt/LinkedList.kt
@@ -1,37 +1,34 @@
 package com.aoyagi.kt
 
+data class StationList(var name: String, var next: Int)
+
+var list = arrayOfNulls<StationList>(5)
+
+var top = 2
+
 fun main(args: Array<String>) {
+    initStationList()
+    printStationList()
+}
 
-    var list = arrayOfNulls<StationList>(5)
-
+fun initStationList() {
     list[0] = StationList("新大阪", -1)
     list[1] = StationList("名古屋", 3)
     list[2] = StationList("東京", 4)
     list[3] = StationList("京都", 0)
     list[4] = StationList("新横浜", 1)
 
-    val top = 2
-
-    var idx = top
-    while (idx != -1) {
-        print("[${list[idx]?.name}]->")
-        idx = list[idx]?.next!!
-    }
-    println("")
-
-
-}
-
-data class StationList(var name: String, var next: Int)
-
-
-fun initStationList() {
 
 }
 
 fun printStationList() {
 
-
+    var idx = top
+    while(idx != -1) {
+        print("[${list[idx]?.name}]->")
+        idx = list[idx]?.next!!
+    }
+    println("")
 }
 
 /*
@@ -41,10 +38,14 @@ fun printStationList() {
  */
 fun insertStationList(insIdx: Int, insName: String, prevIdx: Int) {
     list[insIdx]?.name = insName
-    list[insIdx]?.next = list[prevIdx]?.next
+    list[insIdx]?.next = list[prevIdx]?.next!!
     list[prevIdx]?.next = insIdx
 }
 
+/*
+@args delIdx 削除する要素の添字
+@args prevIdx 前の添字
+ */
 fun deleteStationList(delIdx: Int, prevIdx: Int) {
-    list[delIdx]?.next = list[prevIdx]?.next
+    list[delIdx]?.next = list[prevIdx]?.next!!
 }
