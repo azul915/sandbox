e813d09a7d3d3e0983f94f3fa5ff726d6dfe5009
azul915 <k.aoyagi940516@gmail.com>
2019-09-04 02:31:11 +0900

    SearchBinaryTree実装

--- src/com/aoyagi/kt/BinarySearchTree.kt
+++ src/com/aoyagi/kt/BinarySearchTree.kt
@@ -12,7 +12,7 @@ var newIdx = 0
 
 fun main(args: Array<String>) {
 
-    for(i in tree.indices) tree[i] = BST(0, -1, -1)
+    for (i in tree.indices) tree[i] = BST(0,0,0)
 
     addBST(4)
     addBST(6)
@@ -22,6 +22,14 @@ fun main(args: Array<String>) {
     addBST(7)
     addBST(1)
     printPhysicalBST()
+    println("---------------------------------------------")
+    printLogicalBST(rootIdx)
+
+    println("「5」の探索結果 = ${searchBST(5)}")
+    println("「8」の探索結果 = ${searchBST(8)}")
+    println("---------------------------------------------")
+    println("「5」の探索結果 = ${searchRecBST(5, rootIdx)}")
+    println("「8」の探索結果 = ${searchRecBST(8, rootIdx)}")
 }
 
 fun addBST(data: Int) {
@@ -35,14 +43,14 @@ fun addBST(data: Int) {
         do {
             if(data < tree[currentIdx]?.data!!) {
                 if (tree[currentIdx]?.left == -1) {
-                    tree[currentIdx] = BST(data, newIdx, -1)
+                    tree[currentIdx] = BST(tree[currentIdx]?.data!!, newIdx, tree[currentIdx]?.right!!)
                     finished = true
                 } else {
                     currentIdx = tree[currentIdx]?.left!!
                 }
             } else {
                 if(tree[currentIdx]?.right == -1) {
-                    tree[currentIdx] = BST(data, -1, newIdx)
+                    tree[currentIdx] = BST(tree[currentIdx]?.data!!, tree[currentIdx]?.left!!, newIdx)
                     finished = true
                 } else {
                     currentIdx = tree[currentIdx]?.right!!
@@ -56,8 +64,50 @@ fun addBST(data: Int) {
 
 fun printPhysicalBST() {
 
-    for (i in 0..newIdx) {
+    for (i in 0 until newIdx) {
         println("tree[${i}] : data = ${tree[i]?.data}, left = ${tree[i]?.left}, right = ${tree[i]?.right}")
     }
 
+}
+
+// 深さ優先探索
+fun printLogicalBST(currentIdx: Int) {
+
+    if (currentIdx != -1) {
+        println("tree[${currentIdx}] : data = ${tree[currentIdx]?.data!!}, left = ${tree[currentIdx]?.left!!}, right = ${tree[currentIdx]?.right!!}")
+
+        printLogicalBST(tree[currentIdx]?.left!!)
+        printLogicalBST(tree[currentIdx]?.right!!)
+    }
+}
+
+// 二分探索木の探索
+fun searchBST(x: Int): Int {
+    var idx = -1
+    var currentIdx = rootIdx
+
+    while (currentIdx != -1) {
+        if (tree[currentIdx]?.data!! == x) {
+            idx = currentIdx
+            break
+        } else if (tree[currentIdx]?.data!! > x) {
+            currentIdx = tree[currentIdx]?.left!!
+        } else {
+            currentIdx = tree[currentIdx]?.right!!
+        }
+    }
+    return idx
+}
+
+// 再帰による二分探索木の探索
+fun searchRecBST(x: Int, currentIdx: Int): Int {
+    return if (currentIdx == -1) {
+        -1
+    } else {
+        if(tree[currentIdx]?.data!! > x) {
+            searchRecBST(x, tree[currentIdx]?.left!!)
+        } else {
+            searchRecBST(x, tree[currentIdx]?.right!!)
+        }
+    }
 }
