f3b3b940e35719577f08f6fa6d80c893fb213332
azul915 <k.aoyagi940516@gmail.com>
2020-06-25 23:29:31 +0900

    up

--- src/com/aoyagi/kt/algorithm/SequentialSearch.kt
+++ src/com/aoyagi/kt/algorithm/SequentialSearch.kt
@@ -1,11 +1,10 @@
 package com.aoyagi.kt.algorithm
 
 fun main() {
-    val x= readLine()!!.toInt()
     val a = intArrayOf(72, 68, 92, 88, 41, 53, 97, 84, 39, 55)
     var pos = -1
     for (i in a.indices) {
-        if (a[i] == x) {
+        if (a[i] == 53) {
             pos = i
             break
         }
